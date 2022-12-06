ca68f71af440a54ef03922a1e2885ecab984fd5f
Cinthia Katiane <cinthia.cka@gmail.com>
2022-12-05 22:51:55 -0300

    Configuração do vuetify e conexão com a api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     <h1>Clientes</h1>
+    <ul v-for="user in users" :key="user.id">
+      <li>{{ user.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HomeView",
+  data() {
+    return {
+      users: {},
+    };
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      axios
+        .get("http://localhost:3003/clients")
+        .then((res) => {
+          this.users = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
